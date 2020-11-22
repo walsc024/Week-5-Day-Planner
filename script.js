036df16f32d040011179a9f59197bf0a14368d54
christopher walsh <chriswalsh08@googlemail.com>
2020-11-22 13:48:35 +0000

    added local storage get items

--- script.js
+++ script.js
@@ -32,6 +32,21 @@ var nineTask = localStorage.getItem("9")
 $("#9 .description").val(nineTask)
 var tenTask = localStorage.getItem("10")
 $("#10 .description").val(tenTask)
+var elevenTask = localStorage.getItem("11")
+$("#11 .description").val(elevenTask)
+var twelveTask = localStorage.getItem("12")
+$("#12 .description").val(twelveTask)
+var oneTask = localStorage.getItem("1")
+$("#1 .description").val(oneTask)
+var twoTask = localStorage.getItem("2")
+$("#2 .description").val(twoTask)
+var threeTask = localStorage.getItem("3")
+$("#13 .description").val(threeTask)
+var fourTask = localStorage.getItem("4")
+$("#4 .description").val(fourTask)
+var fiveTask = localStorage.getItem("5")
+$("#5 .description").val(fiveTask)
+
 
 
 
