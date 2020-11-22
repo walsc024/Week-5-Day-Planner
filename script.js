7baf9cf73050b1333cea90e8825b754aadaf206c
christopher walsh <chriswalsh08@googlemail.com>
2020-11-22 12:23:04 +0000

    added local storage

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ console.log(presentHour)
 
 
 
+$(".saveBtn").on("click", function(){
+    var task = $(this).siblings(".description").val()
+    console.log(task)
+    var id = $(this).parent().attr("id");
+    localStorage.setItem(id, task)
+})
 
 
+var nineTask = localStorage.getItem("9")
+$("#9 .description").val(nineTask)
+var tenTask = localStorage.getItem("10")
+$("#10 .description").val(tenTask)
 })
